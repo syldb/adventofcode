a2582369f44a93d6c49e33971bad664878861d56
Sylvain <syl.delabye@gmail.com>
2020-12-01 08:28:32 +0100

    Day1 part 2

--- day1.py
+++ day1.py
@@ -1,9 +1,10 @@
 def find_result(report):
     for entry1 in report:
         for entry2 in report:
-            total = int(entry1) + int(entry2)
-            if total == 2020:
-                return int(entry1) * int(entry2)
+            for entry3 in report:
+                total = int(entry1) + int(entry2) + int(entry3)
+                if total == 2020:
+                    return int(entry1) * int(entry2) * int(entry3)
     raise Exception('Entries not found')
 
 if __name__ == '__main__':
@@ -11,8 +12,9 @@ if __name__ == '__main__':
     report = [1721, 979, 366, 299, 675, 1456]
     result = find_result(report)
     print('result example = ', result)
+    assert result == 241861950
 
     with open('input1.txt') as f:
         report = f.readlines()
-        result = find_result(report)
-        print('result final = ', result)
+    result = find_result(report)
+    print('result final = ', result)
