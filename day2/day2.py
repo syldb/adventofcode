e42efebbfe4b4638d1c487c3a78090f36519142f
Sylvain <syl.delabye@gmail.com>
2020-12-02 09:43:06 +0100

    Day2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,27 +1,25 @@
+import re
+
 def count_valid_pwd(data):
     total = 0
     for tup in data:
-        valid = is_valid(tup[3], tup[0], tup[1], tup[2])
+        valid = is_valid_part_2(tup[3], tup[0], tup[1], tup[2])
         if valid:
             total = total + 1
     return total
 
 
-def is_valid(pwd, min_num, max_num, letter):
+def is_valid_part_1(pwd, min_num, max_num, letter):
     counter = pwd.count(letter)
     return counter >= min_num and counter <= max_num
 
+def is_valid_part_2(pwd, pos1, pos2, letter):
+    return (pwd[pos1 - 1] == letter) ^ (pwd[pos2 - 1] == letter)
 
 def str_to_tuple(data):
     for line in data:
-        line_list = line.split(':')
-        pwd = line_list[1].strip()
-        line_list = line_list[0].split(' ')
-        letter = line_list[1].strip()
-        line_list = line_list[0].split('-')
-        min_num = int(line_list[0])
-        max_num = int(line_list[1])
-        yield (min_num, max_num, letter, pwd)
+        tup = re.match(r"(\d+)-(\d+) ([a-z]): ([a-z]*)", line).groups()
+        yield (int(tup[0]), int(tup[1]), tup[2], tup[3])
 
 if __name__ == '__main__':
     print('Start day2...')
@@ -30,9 +28,9 @@ if __name__ == '__main__':
         (1, 3, 'b', 'cdefg'),
         (2, 9, 'c', 'ccccccccc')
     ]
-    result = count_valid_pwd(data)
-    print('result test = ', result)
-    assert result == 2
+    result_test = count_valid_pwd(data)
+    print('result test = ', result_test)
+    assert result_test == 1
     with open('input2.txt') as f:
         raw_data = f.readlines()
     data = str_to_tuple(raw_data)
