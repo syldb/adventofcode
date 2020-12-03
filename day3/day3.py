3a34d91c400d3e2520d1696e8aae7075f5dd1712
Sylvain <syl.delabye@gmail.com>
2020-12-03 09:57:52 +0100

    Day3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -1,4 +1,5 @@
 import itertools
+import functools
 
 
 def count_trees(data, right, down):
@@ -12,15 +13,26 @@ def count_trees(data, right, down):
     return total_trees
 
 
+def count_trees_part_2(data, slope_list):
+    return functools.reduce(
+        lambda acc, slope: acc * count_trees(data, slope[0], slope[1]),
+        slope_list,
+        1
+    )
+
 if __name__ == '__main__':
     print('Start day 3...')
     with open('input3_test.txt') as f:
         test_data = f.readlines()
     test_result = count_trees(test_data, 3, 1)
-    print('test_result =', test_result)
     assert test_result == 7
+    slope_list = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    test_result_2 = count_trees_part_2(test_data, slope_list)
+    assert test_result_2 == 336
     with open('input3.txt') as f:
         data = f.readlines()
     result = count_trees(data, 3, 1)
     print('result = ', result)
+    result_2 = count_trees_part_2(data, slope_list)
+    print('result 2 = ', result_2)
     print('Exit day 3')
