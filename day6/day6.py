d7a7654377ff05b55f9e338aff316eded3629664
Sylvain Delabye <sylvain.delabye@people-doc.com>
2020-12-09 15:11:28 +0100

    Day6 part 2

--- day6/day6.py
+++ day6/day6.py
@@ -6,17 +6,23 @@ def parse(raw_data):
 
 
 def part1(data):
-    data = [len(set(''.join(g))) for g in data]
-    return sum(data)
+    return sum([len(set(''.join(g))) for g in data])
+
+
+def part2(data):
+    # Not readable, but fun to write
+    return sum([len(set.intersection(*[set(x) for x in group])) for group in data])
 
 
 if __name__ == '__main__':
     with open('test_input.txt') as f:
         raw_data = f.read()
     data = parse(raw_data)
-    assert part1(data) == 11
+    # assert part1(data) == 11
+    assert part2(data) == 6
 
     with open('input.txt') as f:
         raw_data = f.read()
     data = parse(raw_data)
-    print('part1: ', part1(data))
+    # print('part1: ', part1(data))
+    print('part2: ', part2(data))
